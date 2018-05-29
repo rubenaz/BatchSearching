e7361b89c8685ad557fd831f6deacab6935b1d0f
rubenaz <rubenazoulay90@gmail.com>
2018-05-29 17:22:41 +0300

    bug song fixed

--- src/app/searching/searching.component.ts
+++ src/app/searching/searching.component.ts
@@ -48,17 +48,18 @@ export class SearchingComponent implements OnInit {
   countSendUrls=0;
   steamResponse=0;
   filmResponse=0;
-  flagGame=true;
-  typed="";
-  countOfColums;
-  searchUrl:any[]=[];
-  mapResponse;
-  plus=false;
-  htmlStr;
-  finalHtml;
-  newType;
-  flag;
-  
+  flagGame=true
+  typed=""
+  countOfColums
+  searchUrl:any[]=[]
+  mapResponse
+  plus=false
+  htmlStr
+  finalHtml
+  newType
+  flag
+  addApiUrl
+  addResult
   
   
   private service=new APIservice();
@@ -75,7 +76,7 @@ onSave(input){
     return;
 
 
-  this.responseArray=[];
+
   this.displayedColumns = ['position', 'name', 'url'];
   this.steamID=[];
   this.allType=[];
@@ -88,10 +89,10 @@ onSave(input){
   this.steamResponse=0;
   this.service.clearTheArrayType()
   this.allSearch= this.service.load(this.input);
-  this.results=[];
   this.otherColumn=this.service.generate(this.otherColumn,this.allSearch.length);
   this.ELEMENT_DATA=this.service.generate(this.ELEMENT_DATA,this.allSearch.length);
   this.responseArray=this.service.generate(this.responseArray,this.allSearch.length);
+  this.results=
   this.results=Array.of(this.results);
   this.countOfColums=0;
   this.mapResponse=0;
@@ -99,6 +100,8 @@ onSave(input){
   this.finalHtml=[]
   this.plus=false;
   this.flag=false;
+  this.addApiUrl=[];
+  this.addResult=[]
   
 
 
@@ -150,69 +153,78 @@ public  getAnswer(){
           else
             this.flagGame=false;
         }
-        this.loadPage(i);
+        this.loadPage(i,this.results[i]);
         this.pressed=true;
     });
   
   }
 }
 //========================================================================================================================================
-public loadPage(i)
+public loadPage(i,result)
 {
   this.count++;
-      if(this.typed=="photo"){
-        this.responseArray[i][this.countOfColums]= "https://farm" + this.results[i].photos.photo[0].farm + ".staticflickr.com/" + this.results[i].photos.photo[0].server+
-         "/" + this.results[i].photos.photo[0].id +"_" + this.results[i].photos.photo[0].secret +".jpg"
-         console.log(this.responseArray[i]);
+      if(this.typed=="photo" || this.newType=="photo"){
+        this.responseArray[i][this.countOfColums]= "https://farm" + result.photos.photo[0].farm + ".staticflickr.com/" + result.photos.photo[0].server+
+         "/" + result.photos.photo[0].id +"_" + result.photos.photo[0].secret +".jpg"
+         console.log(this.responseArray[i]);     
         //this.responseArray[i]=this.results[i].Image;
+        if(this.plus==false){
         this.ELEMENT_DATA[i][this.countOfColums]={position:i,name:this.allSearch[i],url:this.responseArray[i][this.countOfColums],otherColumns:this.otherColumn[i][this.countOfColums]};//push element in the Element array 
         this.dataSource=new MatTableDataSource(this.ELEMENT_DATA);//push into the table
-      }
-      else if(this.typed=="song")
+      }}
+      else if(this.typed=="song" || this.newType=="song")
       {
-        this.responseArray[i][this.countOfColums]= "https://www.youtube.com/embed/" +this.results[i].items[0].id.videoId;
+        this.responseArray[i][this.countOfColums]= "https://www.youtube.com/embed/" +result.items[0].id.videoId;
         this.responseArray[i][this.countOfColums]=this.sanitizer.bypassSecurityTrustResourceUrl(this.responseArray[i][this.countOfColums]);
         this.ELEMENT_DATA[i][this.countOfColums]={position:i,name:this.allSearch[i],url:this.responseArray[i][this.countOfColums],otherColumns:this.otherColumn[i][this.countOfColums]};
-
+        this.dataSource=new MatTableDataSource(this.ELEMENT_DATA);
       }
-     else if(this.typed=="wiki"){
-       this.responseArray[i]=this.results[i][2];
+     else if(this.typed=="wiki" || this.newType=="wikipedia"){
+       this.responseArray[i]=result[2];
        this.ELEMENT_DATA[i][this.countOfColums]={position:i,name:this.allSearch[i],url:this.responseArray[i][this.countOfColums],otherColumns:this.otherColumn[i][this.countOfColums]};//push element in the Element array 
 
       }
-      else if(this.typed=="film")
+      else if(this.typed=="film"||this.newType=="imdb" || this.newType=="trailer")
       {
-      this.responseArray[i][this.countOfColums]= "https://www.youtube.com/embed/" +this.results[i].items[0].id.videoId;
+      this.responseArray[i][this.countOfColums]= "https://www.youtube.com/embed/" +result.items[0].id.videoId;
       this.responseArray[i][this.countOfColums]=this.sanitizer.bypassSecurityTrustResourceUrl(this.responseArray[i][this.countOfColums]);
       
-      this.http.get("https://cors.io/?https://api.themoviedb.org/3/search/movie?api_key=9949ee3ad75fde21364a3c248c3284f3&query=" + this.allSearch[i] +"&language=en").toPromise().then(response => {
-        let result=response.json();
-        this.otherColumn[i][this.countOfColums]=this.service.getResultFromFilm(result);
-        this.filmResponse++;
-        this.ELEMENT_DATA[i][this.countOfColums]={position:i,name:this.allSearch[i],url:this.responseArray[i][this.countOfColums],otherColumns:this.otherColumn[i][this.countOfColums]};
-        if(this.filmResponse==this.allSearch.length){
-            this.dataSource=new MatTableDataSource(this.ELEMENT_DATA);//push into the table
-            this.flag=true;
-        }
+        this.http.get("https://cors.io/?https://api.themoviedb.org/3/search/movie?api_key=9949ee3ad75fde21364a3c248c3284f3&query=" + this.allSearch[i] +"&language=en").toPromise().then(response => {
+          let res=response.json();
+          this.otherColumn[i][this.countOfColums]=this.service.getResultFromFilm(res);
+          this.filmResponse++;
+          if(this.plus==false)
+          {
+            this.ELEMENT_DATA[i][this.countOfColums]={position:i,name:this.allSearch[i],url:this.responseArray[i][this.countOfColums],otherColumns:this.otherColumn[i][this.countOfColums]};
+            if(this.filmResponse==this.allSearch.length)
+            {
+                this.dataSource=new MatTableDataSource(this.ELEMENT_DATA);//push into the table
+                this.flag=true;
+            }
+          }
         });
       }
-      else if(this.typed=="map")
+      else if(this.typed=="map" || this.newType=="map")
       {
-        this.http.get("https://cors.io/?https://maps.googleapis.com/maps/api/place/details/json?key=AIzaSyDntIUhIrk3e1FjrOEy_EwO7bFrSCt3Eos&placeid=" + this.results[i].results[0].place_id).toPromise().then(response => {
-            let result=response.json();
+        this.http.get("https://cors.io/?https://maps.googleapis.com/maps/api/place/details/json?key=AIzaSyDntIUhIrk3e1FjrOEy_EwO7bFrSCt3Eos&placeid=" + result.results[0].place_id).toPromise().then(response => {
+            let res=response.json();
             this.mapResponse++;
-            this.responseArray[i][this.countOfColums]=result.result.url+"&output=embed";
+            this.responseArray[i][this.countOfColums]=res.result.url+"&output=embed";
             console.log(this.otherColumn)
-      this.responseArray[i][this.countOfColums]=this.sanitizer.bypassSecurityTrustResourceUrl(this.responseArray[i][this.countOfColums]);
-      this.ELEMENT_DATA[i][this.countOfColums]={position:i,name:this.allSearch[i],url:this.responseArray[i][this.countOfColums],otherColumns:this.otherColumn[i][this.countOfColums]};//push element in the Element array 
-        if(this.mapResponse==this.allSearch.length){//if get all the answer of the server
-            this.dataSource=new MatTableDataSource(this.ELEMENT_DATA);//push into the table
-            this.flag=true;
-        }
+            this.responseArray[i][this.countOfColums]=this.sanitizer.bypassSecurityTrustResourceUrl(this.responseArray[i][this.countOfColums]);
+            if(this.plus==false)
+            {
+            this.ELEMENT_DATA[i][this.countOfColums]={position:i,name:this.allSearch[i],url:this.responseArray[i][this.countOfColums],otherColumns:this.otherColumn[i][this.countOfColums]};//push element in the Element array 
+              if(this.mapResponse==this.allSearch.length)
+              {//if get all the answer of the server
+                  this.dataSource=new MatTableDataSource(this.ELEMENT_DATA);//push into the table
+                  this.flag=true;
+              }
+            }
         });
       
       }
-      else if (this.typed=="direction")
+      else if (this.typed=="direction" || this.newType=="direction")
       {
        // this.http.get("https://cors.io/?https://maps.googleapis.com/maps/api/directions/json?key=AIzaSyDntIUhIrk3e1FjrOEy_EwO7bFrSCt3Eos&origin=" + this.results[i].geocoded_waypoints[0].place_id + "&destination=" +this.results[i].geocoded_waypoints[1].place_id ).toPromise().then(response => {
         this.responseArray[i][this.countOfColums]=this.apiUrl[i];
@@ -220,7 +232,7 @@ public loadPage(i)
         this.ELEMENT_DATA[i][this.countOfColums]={position:i,name:this.allSearch[i],url:this.responseArray[i][this.countOfColums],otherColumns:this.otherColumn[i][this.countOfColums]};
      // });//push element in the Element array 
       }
-      else if(this.typed=="game")
+      else if(this.typed=="game" ||  this.newType=="game")
       {
         if(this.flagGame==true){
           this.http.get("https://cors.io/?http://store.steampowered.com/api/appdetails?appids=" + this.steamID[i] +"&key=B458483E2C76C8BE13EB05C37106916A&format=json").toPromise().then(response => {
@@ -236,11 +248,14 @@ public loadPage(i)
                 let youtube_result=response.json();
                 this.otherColumn[i][this.countOfColums]= "https://www.youtube.com/embed/" +youtube_result.items[0].id.videoId;
                 this.otherColumn[i][this.countOfColums]=this.sanitizer.bypassSecurityTrustResourceUrl(this.otherColumn[i][this.countOfColums]);
+                if(this.plus==false)
+                {
                 this.ELEMENT_DATA[i][this.countOfColums]={position:i,name:this.allSearch[i],url:this.responseArray[i][this.countOfColums],otherColumns:this.otherColumn[i][this.countOfColums]};//push element in the Element array 
             
                  if(this.steamResponse==this.allSearch.length){
                    this.dataSource=new MatTableDataSource(this.ELEMENT_DATA);//push into the table*/
                    this.flag=true;}
+                 }
              });
           });
         }
@@ -253,8 +268,12 @@ public loadPage(i)
       }
       if(this.typed!="game" && this.typed!="film")
       {
-          if(this.count==this.allSearch.length){
-            this.dataSource=new MatTableDataSource(this.ELEMENT_DATA[0]);//push into the table
+          if(this.count==this.allSearch.length && this.plus==false)
+          {
+            if(this.typed=="song")
+                this.dataSource=new MatTableDataSource(this.ELEMENT_DATA);
+            else
+                this.dataSource=new MatTableDataSource(this.ELEMENT_DATA[0]);//push into the table
             this.flag=true;
           }
       }
@@ -270,24 +289,41 @@ public loadPage(i)
       return;
     this.countOfColums++;
     this.displayedColumns[this.displayedColumns.length]='otherSearch'+ (this.countOfColums);
-    
     for(let i=0; i<this.allSearch.length;i++)
     {
-   //let url=this.service.returnURL(selectType,this.allSearch[i]);
-
-  /*  this.http.get(this.apiUrl[i]).toPromise().then(response => 
+    this.addApiUrl[i]=this.service.returnURL(selectType,this.allSearch[i]);
+    console.log(this.addApiUrl[i])
+    this.http.get(this.addApiUrl[i]).toPromise().then(response => 
       {
-
-      });*/
-      if(this.countOfColums==1)
+        
+          if(/*this.typed!="map" && */this.typed!="direction" && this.typed!="game")
+            this.addResult[i]=response.json();
+          else if (this.typed=="game")
+          {
+            this.addResult[i]=response;
+            //console.log(response);
+            if(this.service.regex(this.addResult[i]._body)!=null){
+                this.steamID[i]=this.service.regex(this.addResult[i]._body);console.log("this steam id = " + this.steamID[i]);
+            }
+            else
+              this.flagGame=false;
+          }
+          this.loadPage(i,this.addResult[i]);
+      });
+   /* if(selectType=="imdb")
     this.htmlStr[i]=this.sanitizer.bypassSecurityTrustHtml('<iframe width="420" height=\"315\" src=\"https://www.youtube.com/embed/tgbNymZ7vqY\"></iframe></div>');
     else
-    this.htmlStr[i]=this.sanitizer.bypassSecurityTrustHtml('<iframe width="420" height=\"315\" src=\"https://www.youtube.com/embed/tgbNymZ7vqf\"></iframe></div>');
+    this.htmlStr[i]=this.sanitizer.bypassSecurityTrustHtml('<iframe width="420" height=\"315\" src=\"https://www.youtube.com/embed/tgbNymZ7vqf\"></iframe></div>');*/
 
-    console.log(this.otherColumn)
+    //console.log(this.otherColumn)
    // this.otherColumn[i][this.countOfColums]=this.sanitizer.bypassSecurityTrustResourceUrl(this.otherColumn[i][this.countOfColums]);
-    this.otherColumn[i][this.countOfColums]=this.htmlStr[i];
-    this.ELEMENT_DATA[i][this.countOfColums]={position:i,name:this.allSearch[i],url:this.responseArray[i][this.countOfColums],otherColumns:this.otherColumn[i][this.countOfColums]};
+    if(this.newType=="map")
+    {
+        this.htmlStr[i]=this.sanitizer.bypassSecurityTrustHtml('<iframe width="200" height="200" frameborder="0" style="border:0"[src]='+ this.responseArray[i][this.countOfColums]+' allowfullscreen></iframe>');
+        console.log(this.responseArray[i][this.countOfColums])
+        this.otherColumn[i][this.countOfColums]=this.htmlStr[i];
+        this.ELEMENT_DATA[i][this.countOfColums]={position:i,name:this.allSearch[i],url:this.responseArray[i][this.countOfColums],otherColumns:this.otherColumn[i][this.countOfColums]};
+    }
     }
     this.dataSource=new MatTableDataSource(this.ELEMENT_DATA);
     this.plus=true;
@@ -295,9 +331,7 @@ public loadPage(i)
   }
   returncol(i)
   {
-    console.log("otherSearch" + i + "")
-    return "otherSearch" + i + "";
-    
+    return "otherSearch" + i + ""; 
   }
   createRange(number){
     var items: number[] = [];
